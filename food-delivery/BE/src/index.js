dac3abd4a96f67538b33c908369fc71ddb84f6ae
24LP0056 24LP0056 <24LP0056@G301-CC0198.local>
2024-09-04 17:41:52 +0800

    update

--- food-delivery/BE/src/index.js
+++ food-delivery/BE/src/index.js
@@ -2,8 +2,12 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import { userRouter } from "./routes/user.js";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+await mongoose.connect(process.env.MONGODB_URI);
 
 const PORT = process.env.PORT || 8000;
 
@@ -11,6 +15,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use("/user", userRouter);
 
 app.get("/", (_, res) => {
   return res.status(200).json("hello world");
