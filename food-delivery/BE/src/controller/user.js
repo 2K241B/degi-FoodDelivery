bcc1e15fc0cfbfef391b145a1d0bbeb1db3569ac
24LP0056 24LP0056 <24LP0056@G301-CC0198.local>
2024-09-05 17:13:10 +0800

    be schema validation

--- food-delivery/BE/src/controller/user.js
+++ food-delivery/BE/src/controller/user.js
@@ -7,18 +7,17 @@ export const createUser = async (req, res) => {
   const { email, name, password, phoneNumber, role } = req.body;
 
   try {
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-      bcrypt.hash(password, salt, async (err, hash) => {
-        const response = await userModel.create({
-          email,
-          name,
-          password: hash,
-          phoneNumber,
-          role,
-        });
-        res.status(200).send(response);
-      });
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+
+    const response = await userModel.create({
+      email,
+      name,
+      password: hash,
+      phoneNumber,
+      role,
     });
+    res.status(200).send(response);
   } catch (error) {
     console.error(error);
     res.status(500).send(error.message);
