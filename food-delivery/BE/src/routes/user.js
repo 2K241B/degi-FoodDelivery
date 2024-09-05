9f8495216051e98bd38316a79c9e625558ff99d9
24LP0056 24LP0056 <24LP0056@G301-CC0198.local>
2024-09-05 14:51:31 +0800

    get all users

--- food-delivery/BE/src/routes/user.js
+++ food-delivery/BE/src/routes/user.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { createUser, getUser, userDelete } from "../controller/user.js";
+import {
+  createUser,
+  getAllUsers,
+  getUser,
+  userDelete,
+} from "../controller/user.js";
 
 const userRouter = Router();
 
 userRouter
   .post("/create", createUser)
   .get("/:id", getUser)
-  .delete("/:id", userDelete);
+  .delete("/:id", userDelete)
+  .get("/", getAllUsers);
 
 export { userRouter };
