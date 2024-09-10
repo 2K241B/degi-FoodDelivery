f6427afff69e0ab60eeb19e07b307a4dc45be06b
24LP0056 24LP0056 <24LP0056@G301-CC0198.local>
2024-09-10 17:03:32 +0800

    fprgot password only ui update

--- food-delivery/FE/src/app/authcode/page.js
+++ food-delivery/FE/src/app/authcode/page.js
@@ -9,11 +9,11 @@ export default function Home() {
           <div className="w-full flex flex-col items-center">
             <h1 className="text-[28px]">Нууц үг сэргээх</h1>
           </div>
-
+          <p>Таны example@pinecone.mn хаяг руу сэргээх код илгээх болно.</p>
           <div className="flex flex-col gap-4">
             <div>
-              <p className="text-[14px]">Имэйл </p>
-              <Input placeholder="Имэйл хаягаа оруулна уу" />
+              <p className="text-[14px]">Нууц үг сэргээх код </p>
+              <Input placeholder="**********" />
             </div>
           </div>
           <div className="flex w-full flex-col gap-8  items-center">
@@ -26,5 +26,3 @@ export default function Home() {
     </div>
   );
 }
-
-// Таны example@pinecone.mn хаяг руу сэргээх код илгээх болно.
