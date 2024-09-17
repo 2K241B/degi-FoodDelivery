cb0817b11b0c762120ffa003262f67a5b401bb35
24LP0056 24LP0056 <24LP0056@G301-CC0198.local>
2024-09-17 15:56:58 +0800

    fe-category-feature-component

--- food-delivery/FE/src/app/page.js
+++ food-delivery/FE/src/app/page.js
@@ -1,18 +1,45 @@
 import { CategoryFeature } from "@/components/my components/categoryFeatureComponent";
 import { MainHeaderSection } from "@/components/my components/mainHeaderSection";
+import { SvgBook, SvgClock, SvgFood } from "@/components/my components/svg";
 
 export default function Home() {
   const Data = [
     {
       title: "Хүргэлтийн төлөв хянах",
       text: "Захиалга бэлтгэлийн явцыг хянах",
+      svg: <SvgBook />,
+    },
+    {
+      title: "Шуурхай хүргэлт",
+      text: "Захиалга бэлтгэлийн явцыг хянах",
+      svg: <SvgClock />,
+    },
+    {
+      title: "Эрүүл, баталгаат орц",
+      text: "Захиалга бэлтгэлийн явцыг хянах",
+      svg: <SvgFood />,
+    },
+    {
+      title: "Хоолны өргөн сонголт",
+      text: "Захиалга бэлтгэлийн явцыг хянах",
+      svg: <SvgBook />,
     },
   ];
 
   return (
     <div>
       <MainHeaderSection />
-      <CategoryFeature />
+      <div className="flex w-[1440px] mx-auto px-[120px] py-[122px] justify-between">
+        {Data.map((e, i) => {
+          return (
+            <CategoryFeature
+              title={Data[i].title}
+              text={Data[i].text}
+              svg={Data[i].svg}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- food-delivery/FE/src/components/my components/categoryFeatureComponent.js
+++ food-delivery/FE/src/components/my components/categoryFeatureComponent.js
@@ -1,7 +1,11 @@
 export const CategoryFeature = ({ svg, title, text }) => {
   return (
-    <div>
-      <div></div>
+    <div className="w-[264px] h-[155px] flex flex-col border-[1px] border-solid border-[#D6D8DB] rounded-[16px] gap-[15px] p-4">
+      <div>{svg}</div>
+      <div>
+        <p className="text-[18px] font-bold">{title}</p>
+        <p className="text-[14px]">{text}</p>
+      </div>
     </div>
   );
 };
